6533da0672583d039f34eb87d30b496167ebcc4e
Mickey <1608747+HackingThings@users.noreply.github.com>
2022-01-04 11:14:42 -0800

    added quantization test

---
+++ IColorQuantizer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCBArtHelper
+{
+    /// <summary>
+    /// This interface provides a color quantization capabilities.
+    /// </summary>
+    public interface IColorQuantizer
+    {
+        /// <summary>
+        /// Adds the color to quantizer.
+        /// </summary>
+        /// <param name="color">The color to be added.</param>
+        void AddColor(Color color);
+
+        /// <summary>
+        /// Gets the palette with specified count of the colors.
+        /// </summary>
+        /// <param name="colorCount">The color count.</param>
+        /// <returns></returns>
+        List<Color> GetPalette(Int32 colorCount);
+
+        /// <summary>
+        /// Gets the index of the palette for specific color.
+        /// </summary>
+        /// <param name="color">The color.</param>
+        /// <returns></returns>
+        Byte GetPaletteIndex(Color color);
+
+        /// <summary>
+        /// Gets the color count.
+        /// </summary>
+        /// <returns></returns>
+        Int32 GetColorCount();
+
+        /// <summary>
+        /// Clears this instance.
+        /// </summary>
+        void Clear();
+    }
+}
---
+++ ImageQuantization.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace PCBArtHelper
+{
+    partial class ImageQuantization
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnQuantize = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(952, 691);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // btnQuantize
+            // 
+            this.btnQuantize.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnQuantize.Location = new System.Drawing.Point(25, 370);
+            this.btnQuantize.Name = "btnQuantize";
+            this.btnQuantize.Size = new System.Drawing.Size(184, 29);
+            this.btnQuantize.TabIndex = 1;
+            this.btnQuantize.Text = "Quantize";
+            this.btnQuantize.UseVisualStyleBackColor = true;
+            this.btnQuantize.Click += new System.EventHandler(this.btnQuantize_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pallete:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.comboBox1.Location = new System.Drawing.Point(70, 327);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(139, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnOpenFile.Location = new System.Drawing.Point(25, 279);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(184, 29);
+            this.btnOpenFile.TabIndex = 5;
+            this.btnOpenFile.Text = "Open Image...";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnOpenFile);
+            this.panel1.Controls.Add(this.btnQuantize);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(939, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(233, 691);
+            this.panel1.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(25, 405);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(184, 29);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save result...";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // ImageQuantization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 691);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ImageQuantization";
+            this.Text = "ImageQuantization";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button btnQuantize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
